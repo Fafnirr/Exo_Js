d880985fbc0bb1707ae4c9cb55086d886da96c74
Ryan <r.bartoli@it-students.fr>
2022-01-04 11:16:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let Prenom = prompt('Entrez votre Prénom');
+let Age = prompt('Entrez votre age');
+
+
+if (Prenom == Sofiane && Age > 18){
+    console.log('Welcome');
+}
+
+if (Prenom.length = Age){
+    console.log('vous avez le meme nombre de lettre que votre age');
+}
+
+if (Age > 30 && Age < 50){
+    console.log("vous etes dans la tranche d'Age");
+}
+
+if (Prenom == 'Abby' || Prenom == 'Paul' && Age < 70){
+    console.log('Bienvenu jeune', Prenom, 'agée de', Age);
+}
+
